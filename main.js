a61d3dcb71b6b543f39b4eef65c9b397186a8991
Kadyrberdi <lhojamkulyyev@gmail.com>
2020-10-08 20:46:17 +0300

    fix : change var to let variable

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var money;
-var income;
-var addExpenses;
-var deposit;
-var mission;
-var period;
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
 
 alert("This is my first lesson Js.");
 console.log("Hello World!");
