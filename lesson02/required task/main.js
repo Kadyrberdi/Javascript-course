101415f6dccd920dd0c3da7650bd03485b650535
Kadyrberdi <lhojamkulyyev@gmail.com>
2020-10-10 16:06:23 +0300

    fix: fix required task

--- lesson02/required task/main.js
+++ lesson02/required task/main.js
@@ -1,18 +1,18 @@
 let money = 200;
-let income = 100;
+let income = "freelance";
 let addExpenses =
   "Квартплата, коммуналка, интернет, продукты, развлечение, еда вне дома, шоппинг";
 let deposit = false;
 let mission = 1000;
 let period = 12;
 
-console.log(money);
-console.log(income);
-console.log(deposit);
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log("Период равен " + period + " месяцев" );
 console.log('Цель заработать ' + mission + " долларов");
 console.log(addExpenses.toLowerCase().split('').join(''));
 
-let budgetDay =  (money + income) / 30;
+let budgetDay = money / 30;
 console.log(budgetDay);
