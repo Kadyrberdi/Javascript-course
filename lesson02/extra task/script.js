23ed83efdcfeff0270ee678c770cdb73a7aeaeb2
Kadyrberdi <lhojamkulyyev@gmail.com>
2020-10-10 16:08:00 +0300

    fix: fix extra task

--- lesson02/extra task/script.js
+++ lesson02/extra task/script.js
@@ -1,6 +1,6 @@
-let num = '266219';
+let num = 266219;
 let reducer = (accumulator, currentValue) => accumulator * currentValue;
 //let multiplication = num.split('').reduce(reducer);
 //let result = multiplication ** 3;
 //console.log(result.toString().substring(0, 2));
-console.log((num.split('').reduce(reducer) ** 3).toString().substring(0, 2));
+console.log((num.toString().split('').reduce(reducer) ** 3).toString().substring(0, 2));
