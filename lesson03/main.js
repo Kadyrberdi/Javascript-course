9be4458c37a98a86d4a36dfa92e8312dcdaebfbf
Kadyrberdi <lhojamkulyyev@gmail.com>
2020-10-13 00:32:04 +0300

    feat: c-p files from lesson02

---
+++ lesson03/main.js
@@ -0,0 +1,21 @@
+'use strict';
+const money = 200;
+const income = "freelance";
+const addExpenses =
+  "Квартплата, коммуналка, интернет, продукты, развлечение, еда вне дома, шоппинг";
+const deposit = false;
+const mission = 1000;
+const period = 12;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцев" );
+console.log('Цель заработать ' + mission + " долларов");
+console.log(addExpenses.toLowerCase().split('').join(''));
+
+const budgetDay = money / 30;
+console.log(budgetDay);
+
+const money = 
