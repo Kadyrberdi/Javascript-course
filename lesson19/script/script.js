26ab99c4b7964222b1d944677c3d594a36d8332a
Kadyrberdi <lhojamkulyyev@gmail.com>
2020-11-07 02:53:34 +0300

    feat: add origin files to lesson19

---
+++ lesson19/script/script.js
@@ -0,0 +1,48 @@
+window.addEventListener('DOMContentLoaded', function() {
+  'use strict';
+
+  //timer
+  function countTimer(deadline) { 
+    let timerHours = document.querySelector('#timer-hours'),
+    timerMinutes = document.querySelector('#timer-minutes'),
+    timerSeconds = document.querySelector('#timer-seconds');
+    
+    function  getTimeRemaining() { 
+      let dateStop = new Date(deadline).getTime(),
+      dateNow = new Date().getTime(),
+      timeRemaining = (dateStop - dateNow) / 1000,
+      seconds = Math.floor(timeRemaining % 60),
+      minutes = Math.floor((timeRemaining / 60) % 60),
+      hours = Math.floor(timeRemaining / 60 / 60) % 24;
+      return {timeRemaining, hours, minutes, seconds };
+    }
+
+    // Add Zeros
+    function addZero(n) {
+      return (parseInt(n, 10) < 10 ? "0" : "") + n;
+    }
+    
+    function updateClock() {
+      let timer = getTimeRemaining();
+      let priceSetInterval;
+      
+      timerHours.textContent = addZero(timer.hours);
+      timerMinutes.textContent = addZero(timer.minutes);
+      timerSeconds.textContent = addZero(timer.seconds);
+      
+      if(timer.timeRemaining > 0) {
+        priceSetInterval = setInterval(updateClock, 1000);
+      } else {
+        timerHours.textContent = '00';
+        timerMinutes.textContent = '00';
+        timerSeconds.textContent = '00';
+        clearInterval(priceSetInterval);
+      }
+
+    }    
+    updateClock();
+  }
+
+  countTimer('07 november 2020 02:26:00');
+
+});
