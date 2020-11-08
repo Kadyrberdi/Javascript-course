5e423273381df1d06ee6187b8a33de1c73099372
Kadyrberdi <lhojamkulyyev@gmail.com>
2020-11-08 13:37:39 +0300

    feat: add tabs

--- lesson20/script/script.js
+++ lesson20/script/script.js
@@ -117,14 +117,17 @@ window.addEventListener('DOMContentLoaded', () => {
         const toggleTabContent = index => {
             for (let i = 0; i < tabContent.length; i++) {
                 if (index === i) {
-                    tabContent[i].classList.remove('.d-none');
+                    tabContent[i].classList.remove('d-none');
+                    tab[i].classList.add('active');
                 } else {
-                    tabContent[i].classList.add('.d-none');
+                    tabContent[i].classList.add('d-none');
+                    tab[i].classList.remove('active');
                 }
             }
         };
         tabHeader.addEventListener('click', event => {
-            const target = event.target;
+            let target = event.target;
+            target = target.closest('.service-header-tab'); // если не найдет поднимается на вверх и ищет его родителей
             if (target.classList.contains('service-header-tab')) {
                 tab.forEach((item, i) => {
                     if (item === target) {
@@ -133,7 +136,6 @@ window.addEventListener('DOMContentLoaded', () => {
                 });
             }
         });
-
     };
 
     tabs();
