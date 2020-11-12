6692848888ee8f692885b96708da86b53ca3d897
Kadyrberdi <lhojamkulyyev@gmail.com>
2020-11-12 19:01:27 +0300

    feat: add lesson24

--- lesson24/script/script.js
+++ lesson24/script/script.js
@@ -273,10 +273,57 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const calcItems = document.getElementsByClassName('calc-item');
 
-    for (let i = 1; i < calcItems.length ; i++) {
+    for (let i = 1; i < calcItems.length; i++) {
         calcItems[i].addEventListener('input', () => {
             calcItems[i].value = calcItems[i].value.replace(/\D/g, '');
         });
     }
+
+    // calculator
+
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+        calcType = document.querySelector('.calc-type'),
+        calcСount = document.querySelector('.calc-count'),
+        calcSquare = document.querySelector('.calc-square'),
+        calcCount = document.querySelector('.calc-count'),
+        calcDay = document.querySelector('.calc-day'),
+        totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+
+            totalValue.textContent = total;
+        };
+
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+
+            if (target.matches('select') || target.matches('input')) {
+                countSum();
+            }
+        });
+    };
+
+    calc(100);
 
 });
